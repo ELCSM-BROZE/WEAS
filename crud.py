a39b7086f13647748bf6c133b4e9fd66f31d6a61
ELCSM-BROZE <151604713+ELCSM-BROZE@users.noreply.github.com>
2025-10-24 12:23:22 -0300

    actualizacion
    yeahhhhhh

--- crud.py
+++ crud.py
@@ -66,7 +66,60 @@ def read_persona():
         print(persona)
 
 def update_persona():
-    pass
+    rut = print("")
+    for persona in personas:
+        if persona.rut == rut:
+            while True:
+                print(f"""
+                    ===========================================
+                    ||            Gestor de persona          ||
+                    ===========================================
+                    1.rut: {persona.rut}
+                    2.nombre: {persona.nombre}
+                    3.apellido: {persona.apellido}
+                    4.fecha de nacimiento: {persona.fecha_nacimiento}
+                    5.codigo de area: {persona.cod_area}
+                    6.telefono: {persona.telefono}
+                    0.no seguir modificando
+                    """
+                    )
+                opcion = input("¿Que dato quieres modificar?")
+                if opcion == "1":
+                    rut: str = input("Ingrese el rut de la persona: ")
+                    if persona_existe(persona):
+                        return print(f"La persona con el rut {persona.rut} ya existe. Intenete con otro rut.")
+                    persona.rut = rut
+                    print("Rut modificado correctamente ")
+                elif opcion == "2":
+                    persona.nombre = nombre
+                    nombre: str = input("Ingrese los nombres de la persona: ")
+                    print("Nombre actualizado correctamente")
+                elif opcion == "3":
+                    persona.apellido = apellido
+                    apellido: str = input("Ingrese los apellidos de la persona: ")
+                    print("Apellido actualizado correctamente")
+                elif opcion == "4":
+                    dia_nacimiento: date = date (input("Ingrese dia de nacimiento: "))
+                    mes_nacimiento: date = date (input("Ingrese mes de nacimiento: "))
+                    anio_nacimiento: date = date (input("Ingrese año de nacimiento: "))
+                    fecha_nacimiento: date = date(
+                        year=anio_nacimiento,
+                        month=mes_nacimiento,
+                        day=dia_nacimiento
+                        )
+                    persona.fecha_nacimiento = fecha_nacimiento
+                    print("fecha de nacimiento se actualizo correctamente")
+                elif opcion == "5":
+                    persona.cod_area = cod_area
+                    cod_area: int = int (input("Ingrese codigo de area de su localidad: "))
+                    print("Codigo de area actualizado correctamente ")
+                elif opcion == "6":
+                    persona.telefono = telefono
+                    telefono: int = int (input("Ingrese telefono de la persona: "))
+                    print("Telefono actualizado correctamente ")
+                elif opcion == "0":
+                    break
+    print()
 
 def delete_persona():
     pass
@@ -79,15 +132,18 @@ while True:
         ===========================================
         1.Crear Persona
         2.Lista Personas
-        3.salir
+        3.Actualizar Personas
+        0.salir
         """
     )
-    opcion = input("Ingrese una opcion [1-2-3]: ")
+    opcion = input("Ingrese una opcion [1-2-3-0]: ")
     if opcion == "1":
         create_persona()
     elif opcion == "2":
         read_persona()
     elif opcion == "3":
+        update_persona()
+    elif opcion == "0":
         print("BYE..")
         break
     else:
